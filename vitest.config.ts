03882a7535eac8e0a2e366b0ec9b61efee5d90a7
yingshu0218 <yingshu0218@users.noreply.github.com>
2026-08-05 02:46:35 +0800

    chore(deps): declare workspace dependencies and project references

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,11 +2,21 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    environment: "jsdom",
     include: [
       "tooling/**/*.test.ts",
       "packages/**/*.test.ts",
+      "packages/**/*.test.tsx",
+      "packages/**/test/**/*.test.ts",
+      "packages/**/test/**/*.test.tsx",
       "modules/**/*.test.ts",
+      "modules/**/*.test.tsx",
+      "modules/**/test/**/*.test.ts",
+      "modules/**/test/**/*.test.tsx",
       "examples/**/*.test.ts",
+      "examples/**/*.test.tsx",
+      "examples/**/test/**/*.test.ts",
+      "examples/**/test/**/*.test.tsx",
     ],
   },
 });
